0f51a8bc06608d38f024629fd02a6b968015a83e
Lijian120208 <1301202460@qq.com>
2018-05-23 22:07:45 +0800

    create_db_user
    create database and create users;

---
+++ Myproject/db/db_create/create_db_user.sql
@@ -0,0 +1,4 @@
+﻿create database ljdb default character set utf8 collate utf8_general_ci;
+create user 'lijian'@'%' identified by 'lj120208';
+grant select,insert,update,delete,create on ljdb.* to lijian;
+flush privileges;
